61a4a71d5d9a59d9f6ce1a9eaab07b1d1bc9a4f1
Leonardo Castelo Branco Oliveira <lcbo_7794@hotmail.com>
2017-05-26 00:54:14 -0300

    Preprocessing Update
    Updating methods: cleanData(dataframe), transformData(dataframe) and transformCategoricData(dataframe, c(categoricAtt1, categoricAtt2,...))

--- Preprocessing.R
+++ Preprocessing.R
@@ -105,16 +105,139 @@ cleanData <- function(df){
 transformData <- function(df){
   
   df[["temp_dep"]] <- binning_opt(df[["temperature_dep"]])[[2]]
+  df[["temperature_dep"]] <- NULL
   df[["dew_dep"]] <- binning_opt(df[["dew_pointc_dep"]])[[2]]
+  df[["dew_pointc_dep"]] <- NULL
   df[["pressure_dep"]] <- binning_opt(df[["sea_level_pressurehpa_dep"]])[[2]]
+  df[["sea_level_pressurehpa_dep"]] <- NULL
   df[["humid_dep"]] <- binning_opt(df[["humidity_dep"]])[[2]]
+  df[["humidity_dep"]] <- NULL
   df[["wspeed_dep"]] <- binning_opt(df[["wind_speedkm_h_dep"]])[[2]]
+  df[["wind_speedkm_h_dep"]] <- NULL
   
   df[["temp_arr"]] <- binning_opt(df[["temperature_arr"]])[[2]]
+  df[["temperature_arr"]] <- NULL
   df[["dew_arr"]] <- binning_opt(df[["dew_pointc_arr"]])[[2]]
+  df[["dew_pointc_arr"]] <- NULL
   df[["pressure_arr"]] <- binning_opt(df[["sea_level_pressurehpa_arr"]])[[2]]
+  df[["sea_level_pressurehpa_arr"]] <- NULL
   df[["humid_arr"]] <- binning_opt(df[["humidity_arr"]])[[2]]
+  df[["humidity_arr"]] <- NULL
   df[["wspeed_arr"]] <- binning_opt(df[["wind_speedkm_h_arr"]])[[2]]
+  df[["wind_speedkm_h_arr"]] <- NULL
+  
+  df[["time_dep"]]<-ifelse(strptime(df[["partidaprevista"]], "%Y-%m-%d %H:%M:%S")$hour>=5
+  & strptime(df[["partidaprevista"]], "%Y-%m-%d %H:%M:%S")$hour<=8, "early morning",
+  ifelse(strptime(df[["partidaprevista"]], "%Y-%m-%d %H:%M:%S")$hour>=9
+  & strptime(df[["partidaprevista"]], "%Y-%m-%d %H:%M:%S")$hour<=10, "mid morning",
+  ifelse(strptime(df[["partidaprevista"]], "%Y-%m-%d %H:%M:%S")$hour>=11
+  & strptime(df[["partidaprevista"]], "%Y-%m-%d %H:%M:%S")$hour<=12, "late morning",
+  ifelse(strptime(df[["partidaprevista"]], "%Y-%m-%d %H:%M:%S")$hour>=13
+  & strptime(df[["partidaprevista"]], "%Y-%m-%d %H:%M:%S")$hour<=16, "afternoon",
+  ifelse(strptime(df[["partidaprevista"]], "%Y-%m-%d %H:%M:%S")$hour>=17
+  & strptime(df[["partidaprevista"]], "%Y-%m-%d %H:%M:%S")$hour<=19, "early evening",
+  ifelse(strptime(df[["partidaprevista"]], "%Y-%m-%d %H:%M:%S")$hour>=20
+  & strptime(df[["partidaprevista"]], "%Y-%m-%d %H:%M:%S")$hour<=22, "late evening",
+  ifelse(strptime(df[["partidaprevista"]], "%Y-%m-%d %H:%M:%S")$hour>=23
+  | strptime(df[["partidaprevista"]], "%Y-%m-%d %H:%M:%S")$hour<=4, "night", NA)))))))
+  
+  df[["partidaprevista"]] <- NULL
+  
+  df[["wdirection_dep"]]<-ifelse(df[["winddirdegrees_dep"]]<=11,"N",
+  ifelse(df[["winddirdegrees_dep"]]>=349 & df[["winddirdegrees_dep"]]<=360,"N",
+  ifelse(df[["winddirdegrees_dep"]]>=12 & df[["winddirdegrees_dep"]]<=33,"NNE",
+  ifelse(df[["winddirdegrees_dep"]]>=34 & df[["winddirdegrees_dep"]]<=56,"NE",
+  ifelse(df[["winddirdegrees_dep"]]>=57 & df[["winddirdegrees_dep"]]<=78,"ENE",
+  ifelse(df[["winddirdegrees_dep"]]>=79 & df[["winddirdegrees_dep"]]<=101,"E",
+  ifelse(df[["winddirdegrees_dep"]]>=102 & df[["winddirdegrees_dep"]]<=123,"ESE",
+  ifelse(df[["winddirdegrees_dep"]]>=124 & df[["winddirdegrees_dep"]]<=146,"SE",
+  ifelse(df[["winddirdegrees_dep"]]>=147 & df[["winddirdegrees_dep"]]<=168,"SSE",
+  ifelse(df[["winddirdegrees_dep"]]>=169 & df[["winddirdegrees_dep"]]<=191,"S",
+  ifelse(df[["winddirdegrees_dep"]]>=192 & df[["winddirdegrees_dep"]]<=213,"SSO",
+  ifelse(df[["winddirdegrees_dep"]]>=214 & df[["winddirdegrees_dep"]]<=236,"SO",
+  ifelse(df[["winddirdegrees_dep"]]>=237 & df[["winddirdegrees_dep"]]<=258,"OSO",
+  ifelse(df[["winddirdegrees_dep"]]>=259 & df[["winddirdegrees_dep"]]<=281,"O",
+  ifelse(df[["winddirdegrees_dep"]]>=282 & df[["winddirdegrees_dep"]]<=303,"ONO",
+  ifelse(df[["winddirdegrees_dep"]]>=304 & df[["winddirdegrees_dep"]]<=326,"NO",
+  ifelse(df[["winddirdegrees_dep"]]>=327 & df[["winddirdegrees_dep"]]<=348,"NNO", NA)))))))))))))))))
+  
+  df[["winddirdegrees_dep"]] <- NULL
+  
+  df[["time_arr"]]<-ifelse(strptime(df[["chegadaprevista"]], "%Y-%m-%d %H:%M:%S")$hour>=5
+  & strptime(df[["chegadaprevista"]], "%Y-%m-%d %H:%M:%S")$hour<=8, "early morning",
+  ifelse(strptime(df[["chegadaprevista"]], "%Y-%m-%d %H:%M:%S")$hour>=9
+  & strptime(df[["chegadaprevista"]], "%Y-%m-%d %H:%M:%S")$hour<=10, "mid morning",
+  ifelse(strptime(df[["chegadaprevista"]], "%Y-%m-%d %H:%M:%S")$hour>=11
+  & strptime(df[["chegadaprevista"]], "%Y-%m-%d %H:%M:%S")$hour<=12, "late morning",
+  ifelse(strptime(df[["chegadaprevista"]], "%Y-%m-%d %H:%M:%S")$hour>=13
+  & strptime(df[["chegadaprevista"]], "%Y-%m-%d %H:%M:%S")$hour<=16, "afternoon",
+  ifelse(strptime(df[["chegadaprevista"]], "%Y-%m-%d %H:%M:%S")$hour>=17
+  & strptime(df[["chegadaprevista"]], "%Y-%m-%d %H:%M:%S")$hour<=19, "early evening",
+  ifelse(strptime(df[["chegadaprevista"]], "%Y-%m-%d %H:%M:%S")$hour>=20
+  & strptime(df[["chegadaprevista"]], "%Y-%m-%d %H:%M:%S")$hour<=22, "late evening",
+  ifelse(strptime(df[["chegadaprevista"]], "%Y-%m-%d %H:%M:%S")$hour>=23
+  | strptime(df[["chegadaprevista"]], "%Y-%m-%d %H:%M:%S")$hour<=4, "night", NA)))))))
+  
+  df[["chegadaprevista"]] <- NULL
+
+  df[["wdirection_arr"]]<-ifelse(df[["winddirdegrees_arr"]]<=11,"N",
+  ifelse(df[["winddirdegrees_arr"]]>=349 & df[["winddirdegrees_arr"]]<=360,"N",
+  ifelse(df[["winddirdegrees_arr"]]>=12 & df[["winddirdegrees_arr"]]<=33,"NNE",
+  ifelse(df[["winddirdegrees_arr"]]>=34 & df[["winddirdegrees_arr"]]<=56,"NE",
+  ifelse(df[["winddirdegrees_arr"]]>=57 & df[["winddirdegrees_arr"]]<=78,"ENE",
+  ifelse(df[["winddirdegrees_arr"]]>=79 & df[["winddirdegrees_arr"]]<=101,"E",
+  ifelse(df[["winddirdegrees_arr"]]>=102 & df[["winddirdegrees_arr"]]<=123,"ESE",
+  ifelse(df[["winddirdegrees_arr"]]>=124 & df[["winddirdegrees_arr"]]<=146,"SE",
+  ifelse(df[["winddirdegrees_arr"]]>=147 & df[["winddirdegrees_arr"]]<=168,"SSE",
+  ifelse(df[["winddirdegrees_arr"]]>=169 & df[["winddirdegrees_arr"]]<=191,"S",
+  ifelse(df[["winddirdegrees_arr"]]>=192 & df[["winddirdegrees_arr"]]<=213,"SSO",
+  ifelse(df[["winddirdegrees_arr"]]>=214 & df[["winddirdegrees_arr"]]<=236,"SO",
+  ifelse(df[["winddirdegrees_arr"]]>=237 & df[["winddirdegrees_arr"]]<=258,"OSO",
+  ifelse(df[["winddirdegrees_arr"]]>=259 & df[["winddirdegrees_arr"]]<=281,"O",
+  ifelse(df[["winddirdegrees_arr"]]>=282 & df[["winddirdegrees_arr"]]<=303,"ONO",
+  ifelse(df[["winddirdegrees_arr"]]>=304 & df[["winddirdegrees_arr"]]<=326,"NO",
+  ifelse(df[["winddirdegrees_arr"]]>=327 & df[["winddirdegrees_arr"]]<=348,"NNO", NA)))))))))))))))))
+  
+  df[["winddirdegrees_arr"]] <- NULL 
+  
+  df[["mes"]][df[["mes"]] == 1] <- "Janeiro"
+  df[["mes"]][df[["mes"]] == 2] <- "Fevereiro"
+  df[["mes"]][df[["mes"]] == 3] <- "Março"
+  df[["mes"]][df[["mes"]] == 4] <- "Abril"
+  df[["mes"]][df[["mes"]] == 5] <- "Maio"
+  df[["mes"]][df[["mes"]] == 6] <- "Junho"
+  df[["mes"]][df[["mes"]] == 7] <- "Julho"
+  df[["mes"]][df[["mes"]] == 8] <- "Agosto"
+  df[["mes"]][df[["mes"]] == 9] <- "Setembro"
+  df[["mes"]][df[["mes"]] == 10] <- "Outubro"
+  df[["mes"]][df[["mes"]] == 11] <- "Novembro"
+  df[["mes"]][df[["mes"]] == 12] <- "Dezembro"
+  
+  df[["diasemana"]][df[["diasemana"]] == 0] <- "Domingo"
+  df[["diasemana"]][df[["diasemana"]] == 1] <- "Segunda-Feira"
+  df[["diasemana"]][df[["diasemana"]] == 2] <- "Terça-Feira"
+  df[["diasemana"]][df[["diasemana"]] == 3] <- "Quarta-Feira"
+  df[["diasemana"]][df[["diasemana"]] == 4] <- "Quinta-Feira"
+  df[["diasemana"]][df[["diasemana"]] == 5] <- "Sexta-Feira"
+  df[["diasemana"]][df[["diasemana"]] == 6] <- "Sábado"
+  
+  return(df)
+  
+}
+
+transformCategoricData <- function(df, categoricData){
+  
+  categorics <- array(categoricData, dim = c(1, length(categoricData))) 
+  
+  for (att in 1:ncol(categorics)){
+    
+     x <- model.matrix(as.formula(paste("~", paste(categorics[att], "-1"))), data=df)
+     
+     df <- cbind(df, x)
+     
+     df[[categorics[att]]] <- NULL
+    
+  }
   
   return(df)
   
