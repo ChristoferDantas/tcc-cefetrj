baeafd55f24c8cb5b59ec1270edb33f9b4fb41e5
ChristoferDantas <christoferdantas81@gmail.com>
2017-06-19 20:55:47 -0300

    Update BestConfiguration_mlp.R
    Permitido a configuração dos parâmetros

--- BestConfiguration_mlp.R
+++ BestConfiguration_mlp.R
@@ -30,3 +30,33 @@ bestConfiguration <- function(data, clabel) {
   
   return (c(bestMeanVar, bestDecay, bestNeurons))
 }
+
+bestConfiguration_set <- function(data, clabel, decays = c(0.01, 0.001), neurons = c(3, 4, 5, 6, 7, 8, 9, 10)) {
+  
+  folds <- sample.stratified_kfold(data, clabel, k = 10)
+  
+  bestMeanVar <- -9999
+  bestDecay <- NULL
+  bestNeurons <- NULL
+  
+  MAX_LOOP <- length(decays) * length(neurons)
+  ACTUAL_LOOP <- 1
+  
+  for (n in neurons) {
+    for(d in decays) {
+      toPrint <- sprintf('Loop %i de %i', ACTUAL_LOOP, MAX_LOOP)
+      print(toPrint)
+      
+      meanVar <- cross_val(folds, clabel, neurons = n, decay = d, iterations = 5000, MaxNWts = 3000)
+      
+      if(meanVar > bestMeanVar) {
+        bestMeanVar = meanVar
+        bestDecay = d
+        bestNeurons = n
+      }
+      ACTUAL_LOOP <- ACTUAL_LOOP + 1  
+    }
+  }
+  
+  return (c(bestMeanVar, bestDecay, bestNeurons))
+}
